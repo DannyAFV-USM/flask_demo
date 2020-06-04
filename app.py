1cc6265a2460973d3c45d7a98732243035dde586
Daniela <daniela.fuentesv@usm.cl>
2020-06-03 22:08:22 -0400

    primera versión de la app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello word")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/') #(decoradores)ruta dentro de la carpeta working directory donde se encontrará la pág web
+def index():
+    return "Hola mundo"
+
+if __name__ == "__main__":
+    app.run()
